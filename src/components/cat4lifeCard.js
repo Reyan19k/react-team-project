cf5f9375aa3c809dbcc8a484a4786e4ccd73f504
Reyan <dinreyanud@gmail.com>
2022-10-06 10:54:24 +0100

    added required pages and components

---
+++ src/components/cat4lifeCard.js
@@ -0,0 +1,9 @@
+const CatsForLifeCard = () => {
+    return (
+        <div>
+            <p>Cats for Life Card</p>
+        </div>
+    )
+}
+
+export default CatsForLifeCard;
---
+++ src/components/cats4life.js
@@ -0,0 +1,15 @@
+import CatsForLifeCard from "./cat4lifeCard";
+
+const CatsForLife = () => {
+    return (
+        <div>
+            <div>
+                <CatsForLifeCard
+                />
+            </div>
+            <p>Cats for Life</p>
+        </div>
+    )
+}
+
+export default CatsForLife;
