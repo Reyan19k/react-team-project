631d6bf939bae0a4971053ffff99a02aef7ed84e
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-07 11:54:56 +0100

    added text to side bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import CatsForLife from './components/cats4life';
+import styled from "styled-components"
+import { useState } from "react"
+import Sidebar from "./components/Sidebar"
 import './App.css';
 
 function App() {
+  const [show, setShow] = useState(false)
   return (
-    <div>
+    <Container>
+      <Sidebar show={show} />
+      <StyledButton onClick={() => setShow(!show)}>Show/Hide Cart</StyledButton>
       <CatsForLife />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+display: flex;
+height: 100vh;
+width: 100vw;`
+
+const StyledButton = styled.button`
+border: none;
+height: 50px;
+width: 100px;
+background-color: tan;
+color: white;
+border-radius: 5px;
+margin: 10px;
+margin-right: 15px;
+cursor: pointer;
+`
---
+++ src/components/Sidebar.js
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+
+const Sidebar = ({show}) => {
+    return (
+    <BarContainer show={show}>
+        <Sideheader>Your Cart</Sideheader>
+        <Sidecontent></Sidecontent>
+        <Sidefooter>Purchase Cart?</Sidefooter>
+    </BarContainer>
+    )
+}
+
+export default Sidebar
+
+const BarContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+max-height: 100%;
+width: 250px;
+margin-right: 0;
+background-color: ${({show}) => show ? "tan" : "white"};
+transition: all 0.5s;
+transform: ${({show}) => show ? "translateX(0)" : "translateX(-500px)"}
+`
+
+const Sidecontent = styled.div`
+position: absolute;
+width: 100%;
+top: 60px;
+bottom: 60px;
+left: 5px;
+overflow-y:auto;`
+
+const Sideheader = styled.p`
+height: 50px;
+font-size: 35px;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+padding-bottom: 5px;
+margin: 0;
+cursor: default;
+border-bottom: 3px solid black;
+`
+const Sidefooter = styled.div`
+height: 50px;
+font-size: 35px;
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: flex-end;
+paddint-top: 5px;
+margin: 0;
+cursor: pointer;
+border-top: 3px solid black`
