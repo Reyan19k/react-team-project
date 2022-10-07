2a5122664053f5711593ef17a07fa5d43ddd3c65
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-07 12:06:58 +0100

    changed CSS of purchase button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,14 +24,6 @@ transition: all 0.5s;
 transform: ${({show}) => show ? "translateX(0)" : "translateX(-500px)"}
 `
 
-const Sidecontent = styled.div`
-position: absolute;
-width: 100%;
-top: 60px;
-bottom: 60px;
-left: 5px;
-overflow-y:auto;`
-
 const Sideheader = styled.p`
 height: 50px;
 font-size: 35px;
@@ -44,14 +36,23 @@ margin: 0;
 cursor: default;
 border-bottom: 3px solid black;
 `
+const Sidecontent = styled.div`
+position: absolute;
+width: 100%;
+top: 60px;
+bottom: 60px;
+left: 5px;
+overflow-y:auto;`
+
 const Sidefooter = styled.div`
 height: 50px;
 font-size: 25px;
-width: 100%;
+width: 99%;
 display: flex;
 justify-content: center;
 align-items: center;
 paddint-top: 5px;
 margin: 0;
 cursor: pointer;
-border-top: 3px solid black`
+border: 1px solid black;
+background-color: yellow;`
