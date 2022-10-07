3416a94c29cc0260f8f0e64df0e47ab43ee2dd64
Scott Balfour <scottbalf@gmail.com>
2022-10-07 12:32:45 +0100

    footer update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CatsForLife from './components/cats4life';
+import CatsFooter from './components/footer';
 import './App.css';
 
 function App() {
   return (
     <div>
       <CatsForLife />
+      <div><CatsFooter/></div>
     </div>
   );
 }
