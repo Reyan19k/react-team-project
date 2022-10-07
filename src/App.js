a4bf75fc9d550772fecf81bf9cc8a81198193636
Reyan <dinreyanud@gmail.com>
2022-10-07 15:17:24 +0100

    added api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import CatsForLife from './components/cats4life';
 import CatsFooter from './components/footer';
 import NavBar from './components/navbar';
 import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [cats, setCats] = useState()
+  
+  useEffect(() => {
+    async function getCats(){
+      const res = await fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_NU5mYCQqNl9PSQh4QNh8xShqFbfp3eMXKwMQ9szUiYZxaoxjQsyKoDYJJA0dTbw2")
+      const data = await res.json()
+      console.log(data)
+      setCats([...data])
+    }
+    getCats()
+  },[])
+
   return (
     <div>
       <div>
